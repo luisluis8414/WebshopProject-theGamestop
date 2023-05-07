5c648fcfda4be3ec9fa48a2afbec4012fe872a8b
luisluis8414 <luis.wehrberger2@gmail.com>
2023-05-07 18:13:25 +0200

    Clean up

--- MainPage.php
+++ MainPage.php
@@ -18,10 +18,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="extern/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script defer src="extern/bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
     <title>Logged in</title>
 </head>
 <body>
-    
-    <a href="?logout=1">Log out</a>
+
+
+<footer><a href="?logout=1">Log out</a></footer>    
 </body>
 </html>
